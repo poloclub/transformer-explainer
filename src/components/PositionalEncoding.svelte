a56131efd768493ea2316b0a084bc2ab033ee446
Alexander Karpekov <akarpekov3@gatech.edu>
2024-05-24 17:27:05 -0400

    pos enc: change orientation, edit formula

--- src/components/PositionalEncoding.svelte
+++ src/components/PositionalEncoding.svelte
@@ -1,62 +1,49 @@
 <script>
 	import { rgb } from 'd3';
+	import * as d3 from 'd3';
   import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { tokens } from '~/store';
-  import Sankey from '~/components/Sankey.svelte';
 
   const numPositions = 100;
   const fullEmbeddingDim = 768; // Compute 768 dimensions
-  const visualEmbeddingDim = 200; // Truncate to 80 dimensions for visualization
+  const visualEmbeddingDim = 100; // Truncate to 100 dimensions for visualization
 
+	let numBarcodeLines = 21;
 
-  function computePositionalEncodings(numPositions, fullEmbeddingDim, visualEmbeddingDim) {
-    const posEncodings = [];
-    for (let pos = 0; pos < numPositions; pos++) {
-      const posEncoding = [];
-      for (let i = 0; i < fullEmbeddingDim; i++) {
-        if (i % 2 === 0) {
-          posEncoding.push(Math.sin(pos / Math.pow(10000, i / fullEmbeddingDim)));
-        } else {
-          posEncoding.push(Math.cos(pos / Math.pow(10000, (i - 1) / fullEmbeddingDim)));
-        }
-      }
-      posEncodings.push(posEncoding.slice(0, visualEmbeddingDim)); // Keep only the first XX dimensions
-    }
-    return posEncodings;
-  }
+	function computePositionalEncodings(numPositions, fullEmbeddingDim, visualEmbeddingDim) {
+		const posEncodings = Array(visualEmbeddingDim).fill().map(() => Array(numPositions).fill(0));
+
+		for (let pos = 0; pos < numPositions; pos++) {
+			for (let i = 0; i < fullEmbeddingDim; i++) {
+				const value = (i % 2 === 0)
+					? Math.sin(pos / Math.pow(10000, i / fullEmbeddingDim))
+					: Math.cos(pos / Math.pow(10000, (i - 1) / fullEmbeddingDim));
+
+				if (i < visualEmbeddingDim) {
+					posEncodings[i][pos] = value;
+				}
+			}
+		}
+
+		return posEncodings;
+	}
+
 
   let posEncodings = [];
   const hoveredRow = writable(null);
 	const hoveredCol = writable(null);
 
-  // function generateBarcodeArray() {
-	// 	return Array.from({ length: 30 }, () => Math.random());
-  // }
-  // function valueToGrayscale(value) {
-  //   const grayValue = Math.round((1 - value) * 127.5);
-  //   return `rgb(${grayValue}, ${grayValue}, ${grayValue}, 0.7)`;
-  // }
+	// Create a linear color scale
+	const colorScale = d3.scaleLinear()
+		.domain([-1, 0, 1])
+		.range(["rgb(248, 113, 113)", "rgb(255, 255, 255)", "rgb(96, 165, 250)"])
+		.interpolate(d3.interpolateRgb);
 
-  // function getGradientColor(value) {
-	// 	// rgb(48, 111, 171)
-	// 	// rgb(223, 115, 94)
-  //   if (value > 0) {
-  //     return `rgba(48, 111, 171, ${value})`; // Blue gradient
-  //   } else {
-  //     return `rgba(223, 115, 94, ${-value})`; // Red gradient
-  //   }
-  // }
-
-  function getGradientColor(value) {
-		// rgba(248, 113, 113)
-		// rgba(96, 165, 250)
-    if (value > 0) {
-      return `rgba(96, 165, 250, ${value})`; // Blue gradient
-    } else {
-      return `rgba(248, 113, 113, ${-value})`; // Red gradient
-    }
-  }
+	// Function to get the gradient color using D3
+	function getGradientColor(value) {
+		return colorScale(value);
+	}
 
 	posEncodings = computePositionalEncodings(numPositions, fullEmbeddingDim, visualEmbeddingDim);
 
@@ -73,9 +60,9 @@
 				<div class="subcontent flex flex-col">
 					{#each $tokens as token, token_idx}
 						<div class="token-box"
-							on:mouseenter={() => hoveredRow.set(token_idx)}
-							on:mouseleave={() => hoveredRow.set(null)}
-							class:highlight-token-box={token_idx === $hoveredRow}
+							on:mouseenter={() => hoveredCol.set(token_idx)}
+							on:mouseleave={() => hoveredCol.set(null)}
+							class:highlight-token-box={token_idx === $hoveredCol}
 						>
 							<span class="text-ellipsis text-center text-gray-900">{token}</span>
 						</div>
@@ -86,10 +73,10 @@
 				<div class="subtitle">Position</div>
 				<div class="subcontent flex flex-col">
 					{#each $tokens as token, token_idx}
-						<div class="token-box"
-							on:mouseenter={() => hoveredRow.set(token_idx)}
-							on:mouseleave={() => hoveredRow.set(null)}
-							class:highlight-token-box={token_idx === $hoveredRow}
+						<div class="position-box"
+							on:mouseenter={() => hoveredCol.set(token_idx)}
+							on:mouseleave={() => hoveredCol.set(null)}
+							class:highlight-token-box={token_idx === $hoveredCol}
 						>
 							<span class="text-ellipsis text-center text-gray-900">{token_idx}</span>
 						</div>
@@ -100,17 +87,17 @@
 				<div class="subtitle">Embedding</div>
 				<div class="subcontent flex flex-col">
 					{#each $tokens as token, token_idx}
-						<div class="token-box barcode-box flex justify-between"
+						<div class="barcode-box flex flex-col justify-between"
 
-							on:mouseenter={() => hoveredRow.set(token_idx)}
-							on:mouseleave={() => hoveredRow.set(null)}
-							class:highlight-token-box={token_idx === $hoveredRow}
+							on:mouseenter={() => hoveredCol.set(token_idx)}
+							on:mouseleave={() => hoveredCol.set(null)}
+							class:highlight-token-box={token_idx === $hoveredCol}
 						>
-							{#each Array(41) as _, barcode_idx}
+							{#each Array(numBarcodeLines) as _, barcode_idx}
 							  <div class="barcode-bar w-full h-full"
-								on:mouseenter={() => hoveredCol.set(barcode_idx)}
-								on:mouseleave={() => hoveredCol.set(null)}
-								style="background-color: {getGradientColor(posEncodings[token_idx][barcode_idx])};"
+								on:mouseenter={() => hoveredRow.set(barcode_idx)}
+								on:mouseleave={() => hoveredRow.set(null)}
+								style="background-color: {getGradientColor(posEncodings[barcode_idx][token_idx])};"
 								></div>
 							{/each}
 						</div>
@@ -121,9 +108,17 @@
 		<div class="viz-container">
 			<div class="subtitle">Encoding Matrix</div>
 			<div class="subcontent viz-subcontent">
+
 				<div class="chart-container mx-auto">
+					<div class="embed-dim-axis">
+						{#each Array(visualEmbeddingDim + 1).fill(0) as _, index}
+							{#if index % 10 === 0}
+								<div>{index}</div>
+							{/if}
+						{/each}
+					</div>
 					<div class="position-axis">
-						{#each Array(numPositions+1).fill(0) as _, index}
+						{#each Array(numPositions + 1).fill(0) as _, index}
 							{#if index % 10 === 0}
 								<div>{index}</div>
 							{/if}
@@ -131,22 +126,21 @@
 					</div>
 					<div class="matrix">
 						{#each posEncodings as row, rowIndex}
-							<div
-								class="matrix-row"
-								on:mouseenter={() => hoveredRow.set(rowIndex)}
-								on:mouseleave={() => hoveredRow.set(null)}
-								class:matrix-row-highlight={rowIndex === $hoveredRow}
-							>
-								{#each row as value, colIndex}
-									<div
-										class="matrix-cell {($hoveredRow === rowIndex) ? 'highlighted-row' : ''}"
-										style="background-color: {getGradientColor(value)};"
-										on:mouseenter={() => hoveredCol.set(colIndex)}
-										on:mouseleave={() => hoveredCol.set(null)}
-
-									></div>
-								{/each}
-							</div>
+							{#each row as value, colIndex}
+								<div
+									class="matrix-cell {($hoveredCol === colIndex) ? 'highlighted-col' : ''}"
+									class:barcode-row-highlight={rowIndex === $hoveredRow & $hoveredCol === colIndex}
+									style="background-color: {getGradientColor(value)};"
+									on:mouseenter={() => {
+										hoveredRow.set(rowIndex);
+										hoveredCol.set(colIndex);
+									}}
+									on:mouseleave={() => {
+										hoveredRow.set(null);
+										hoveredCol.set(null);
+									}}
+								></div>
+							{/each}
 						{/each}
 					</div>
 					<div class="gradient-legend-container">
@@ -158,34 +152,43 @@
 						</div>
 					</div>
 				</div>
+
+
 				<div class="formula-container flex-col justify-around gap-1 p-2 bg-gray-50 w-[400] mt-4">
-					<div class="even-pos flex flex-1 gap-4 justify-start h-10 p-1 items-center">
+					<div class="even-pos flex flex-1 gap-4 justify-between h-10 p-1 items-center">
 						<div class="flex gap-1 justify-center items-center">
 							<span class="flex "></span>
 							{#if $hoveredCol % 2 === 0}
-								<span class="w-8 flex justify-center">sin</span>
+								<span class="w-8 flex justify-center border border-blue-400 rounded-sm">sin</span>
 							{:else}
-								<span class="w-8 flex justify-center">cos</span>
+								<span class="w-8 flex justify-center border border-blue-400 rounded-sm">cos</span>
 							{/if}
 							<span>(</span>
-					    <span class="w-8 flex justify-center">{$hoveredCol ? $hoveredCol : 'pos'}</span>
-					   	<span>/ 10000 ^ ( 2 *</span>
-							<span class="w-8 flex justify-center">{$hoveredRow ? $hoveredRow : 'i'}</span>
-							<span>/ 768 ) )</span>
-							<span>=</span>
+					    <span class="w-8 flex justify-center border border-blue-400 rounded-sm">{$hoveredCol ? $hoveredCol : 'pos'}</span>
+							<span>/ 10000 </span>
+							<sup>(2 *
+								<span class="border border-blue-400 rounded-sm pl-2 pr-2 mr-1">{$hoveredRow ? $hoveredRow : ' i '}</span>
+							 / 768)</sup>
+							<span>)</span>
+					   	<!-- <span>/ 10000 ^ ( 2 *</span>
+							<sup>superscript hrllo</sup>
+							<span class="w-8 flex justify-center border border-blue-400 rounded-sm">{$hoveredRow ? $hoveredRow : 'i'}</span>
+							<span>/ 768 ) )</span> -->
+							<!-- <span>=</span> -->
 						</div>
+						<!-- <div>=</div> -->
 						{#if $hoveredCol !== null && $hoveredRow !== null}
-						<div class="formula-solution-box w-[80] rounded-sm p-2 items-center"
-						  style="background-color: {getGradientColor(posEncodings[$hoveredRow][$hoveredCol])};
-							border: 3px solid {getGradientColor(posEncodings[$hoveredRow][$hoveredCol])};
-							color: {Math.abs(posEncodings[$hoveredRow][$hoveredCol]) > 0.3 ? 'white' : 'black'};"
-						>
-							{posEncodings[$hoveredRow][$hoveredCol].toFixed(2)}
-						</div>
+							<div>=</div>
+							<div class="formula-solution-box w-[80] rounded-sm p-2 items-center"
+								style="background-color: {getGradientColor(posEncodings[$hoveredRow][$hoveredCol])};
+								border: 3px solid {getGradientColor(posEncodings[$hoveredRow][$hoveredCol])};
+								color: {Math.abs(posEncodings[$hoveredRow][$hoveredCol]) > 0.3 ? 'white' : 'black'};"
+							>
+								{posEncodings[$hoveredRow][$hoveredCol].toFixed(2)}
+							</div>
 						{:else}
-						<div class="formula-solution-box rounded-sm p-2 border-gray-300 border-2 items-center">
-							<span class="opacity-0"></span>
-						</div>
+							<div class="formula-solution-box rounded-sm p-2 border-gray-300 border-2 items-center">
+							</div>
 						{/if}
 					</div>
 				</div>
@@ -197,31 +200,12 @@
 </div>
 
 
-
-
 <style lang="scss">
 
   .highlight {
 		cursor: pointer !important;
 		border: 2px solid theme('colors.gray.500') !important;
 		font-weight: bold;
-	}
-
-  .highlight-token-box {
-		cursor: pointer !important;
-		// border: 2px solid theme('colors.gray.500') !important;
-		font-weight: bold;
-		box-shadow: 0 0 0 1.5px theme('colors.gray.500');
-		transition: 0.1s;
-		// box-shadow: 0 0 0 1px black, 0 0 0 3px blue;
-	}
-
-
-	.barcode-bar:hover{
-		// border: 0.5px solid theme('colors.gray.100');
-		box-shadow: 0 0 0 1px theme('colors.gray.100');
-		transform: scaleY(1.3);
-		transition: 0.1s;
 	}
 
 	.formula-solution-box {
@@ -274,7 +258,7 @@
 				}
 			}
 			.viz-container {
-				flex: 8 0 80%;
+				flex: 6 0 60%;
 				display: flex;
 				flex-direction: column;
 				justify-content: center;
@@ -295,27 +279,20 @@
 		flex: 1 0 0;
 		display: flex;
 		align-items: center;
-		justify-content: start;
+		// justify-content: start;
+		justify-content: space-around;
 	}
 
 	.viz-subcontent {
 		display: flex;
 		flex-direction: column;
-
+		justify-content: start;
 	}
-
-  .chart-container {
-		display: flex;
-		// flex: 8 0 80%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-  }
 
 	.formula-container {
 		display: flex;
-		width: 440px;
+		width: 420px;
+		// width: 600px;
 		height: 80px;
 		border-radius: 4px;
 		border: 1px dashed theme('colors.gray.400');
@@ -328,95 +305,136 @@
 		width: 8rem;
 		height: 2rem;
 		background-color: theme('colors.gray.100');
-		// width: 50px;
-		// height: 50px;
 		border: 1px solid #e2e8f0;
 		border-radius: 4px;
 		margin: 0.5rem;
-
-
 	}
 
-  .matrix {
-		// height: 100%;
-		width: 100%;
-    display: grid;
-    grid-template-columns: repeat(200, 3px); /* 200 columns */
-    grid-template-rows: repeat(100, 3px); /* 100 rows */
+	.position-box {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 2rem;
+		height: 2rem;
+		background-color: theme('colors.gray.100');
+		border: 1px solid #e2e8f0;
+		border-radius: 4px;
+		margin: 0.25rem;
+		font-size: 0.9rem;
+	}
+
+	.barcode-box {
+		width: 2rem;
+		height: 4rem;
+		background-color: theme('colors.gray.100');
+		border: 1px solid #e2e8f0;
+		border-radius: 4px;
+		margin: 0.25rem;
+	}
+
+	.highlight-token-box {
+		cursor: pointer !important;
+		// border: 2px solid theme('colors.gray.500') !important;
+		font-weight: bold;
+		box-shadow: 0 0 0 1.5px theme('colors.gray.500');
+		transition: 0.1s;
+		// box-shadow: 0 0 0 1px black, 0 0 0 3px blue;
+	}
+
+	.barcode-row-highlight {
+		border: 0.2px solid theme('colors.gray.500');
+		transform: scaleX(12);
+		transition: 0.1s;
+		cursor: pointer;
+	}
+
+	.barcode-bar:hover{
+		// border: 0.5px solid theme('colors.gray.100');
+		box-shadow: 0 0 0 1px theme('colors.gray.500');
+		transform: scaleX(1.5);
+		transition: 0.1s;
+	}
+
+  .chart-container {
+		position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+		margin-top: 20px;
   }
 
-  .matrix-row {
-    display: contents;
+  .matrix {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(100, 4px);
+    grid-template-rows: repeat(100, 4px);
   }
 
   .matrix-cell {
-    width: 3px;
-    height: 3px;
+    width: 4px;
+    height: 4px;
     display: inline-block;
-    // transition: transform 0.1s;
   }
 
-	// .matrix-row-highlight {
-	// 	transform: scale(5);
-	// }
-
-  .matrix-cell.highlighted-row {
-    transform: scale(5);
-		// border-top: 1px solid theme('colors.gray.400');
-		// border-bottom: 1px solid theme('colors.gray.400');
-		border-top: 0.5px solid white;
-		border-bottom: 0.5px solid white;
-		// border-radius: 4px;
-		// border: 1px solid theme('colors.gray.400');
-		transition: 0.1s;
-  }
-
-  .matrix-row:hover .tooltip {
-    display: block;
+  .matrix-cell.highlighted-col {
+    transform: scaleX(5);
+		// padding-left: 0.5px solid white;
+		// padding-right: 0.5px solid white;
+		border-left: 0.1px solid theme('colors.gray.600');
+		border-right: 0.1px solid theme('colors.gray.600');
+		// border: 0.1px solid theme('colors.gray.500');
+    transition: 0.1s;
   }
 
   .position-axis {
-		padding-right: 5px;
-    top: 0;
+    padding-right: 5px;
     display: flex;
     flex-direction: column;
-    align-items: center;
-		// height: 100%;
-		height: 320px;
-    justify-content: space-around;
+    justify-content: space-between;
+    height: 100%;
     font-size: 0.5rem;
     color: theme('colors.gray.500');
   }
 
-.gradient-legend-container {
-  display: flex;
-  // flex-direction: column;
-  align-items: center;
-  height: 300px;
-	// height: 100%;
-	margin-left: 20px;
-}
+	.embed-dim-axis {
+		position: absolute;
+		top: 0;
+		left: 22px;
+		display: flex;
+		width: 400px;
+		justify-content: space-between;
+		font-size: 0.5rem;
+		color: theme('colors.gray.500');
+		transform: translateY(-20px); /* Adjust this value to move the axis above the matrix */
+	}
 
-.gradient-legend {
-  width: 8px;
-  height: 100%;
-  background: linear-gradient(to top, rgb(96, 165, 250), white, rgb(248, 113, 113));
-  border: 1px solid theme('colors.gray.300');
-}
+  .gradient-legend-container {
+    display: flex;
+    align-items: center;
+    height: 400px;
+    margin-left: 20px;
+  }
 
-.legend-labels {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-  padding-left: 5px;
-	color: theme('colors.gray.500');
-}
+  .gradient-legend {
+    width: 8px;
+    height: 100%;
+    background: linear-gradient(to top, rgb(96, 165, 250), white, rgb(248, 113, 113));
+    border: 1px solid theme('colors.gray.300');
+  }
 
-.legend-labels span {
-  font-size: 0.55rem; /* Adjust the font size as needed */
-}
+  .legend-labels {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    padding-left: 5px;
+    color: theme('colors.gray.500');
+  }
 
+  .legend-labels span {
+    font-size: 0.55rem; /* Adjust the font size as needed */
+	}
 
 </style>
 
