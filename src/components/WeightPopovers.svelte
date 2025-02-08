1131f450930c25429a8b59814b98a8ff9b067727
aereeeee <dofl5576@gmail.com>
2025-02-08 09:58:01 -0500

    bug fix

--- src/components/WeightPopovers.svelte
+++ src/components/WeightPopovers.svelte
@@ -63,10 +63,6 @@
 				left: softmaxRect.left + scrollLeft,
 				top: softmaxRect.top - topbarHeight
 			};
-			softmaxExpandPos = {
-				left: softmaxRect.left + scrollLeft,
-				top: softmaxRect.top - topbarHeight
-			};
 		};
 
 		setPosition();
