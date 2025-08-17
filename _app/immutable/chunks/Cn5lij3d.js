1095ba881502319ffd87be60c23dac7013ae04e8
aereeeee <dofl5576@gmail.com>
2025-08-17 17:40:25 -0400

    Updates

---
+++ _app/immutable/chunks/Cn5lij3d.js
@@ -0,0 +1,1 @@
+import{s as e}from"./Bx9int6H.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.BbDAXdXj.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Bx9int6H.js";export{o as load_css,r as start};
